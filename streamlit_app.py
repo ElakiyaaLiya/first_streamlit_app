5bb035e8581a88c914acbf7ec6d90c7bc4529be8
ElakiyaaLiya <140605096+ElakiyaaLiya@users.noreply.github.com>
2023-07-26 17:26:20 +0530

    Update streamlit_app.py
    Added emojis to texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,8 @@
 import streamlit
 streamlit.title('My favourite language is French')
 streamlit.header('Simple Words in French')
-streamlit.text('Bonjour Madame')
-streamlit.text('A bientot')
-streamlit.text('Bienvenue')
-streamlit.text('Merci beaucoup')
-streamlit.text('Ca va bien')
+streamlit.text('Bonjour Madame 🥰')
+streamlit.text('A bientot 🤩')
+streamlit.text('Bienvenue 😇')
+streamlit.text('Merci beaucoup 😊')
+streamlit.text('Ca va bien 😎')
