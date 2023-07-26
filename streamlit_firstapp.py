c060d1377bc882b7e9b633774b9c5934b8088ce9
ElakiyaaLiya <140605096+ElakiyaaLiya@users.noreply.github.com>
2023-07-26 17:42:32 +0530

    Update streamlit_firstapp.py
    Imported pandas

--- streamlit_firstapp.py
+++ streamlit_firstapp.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Build your our favourite smoothie 🍸🍹')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
