65405091171a93a875ba4731267912a121001e6b
ElakiyaaLiya <140605096+ElakiyaaLiya@users.noreply.github.com>
2023-07-26 17:20:41 +0530

    Update streamlit_app.py
    Added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,8 @@
 import streamlit
 streamlit.title('My favourite language is French')
+streamlit.header('Simple Words in French')
+streamlit.text('Bonjour Madame')
+streamlit.text('A bientot')
+streamlit.text('Bienvenue')
+streamlit.text('Merci beaucoup')
+streamlit.text('Ca va bien')
